225c16d15f026384214de76f394424246b10a4b5
josh muleshi <jmuleshi2@gmail.com>
2021-04-02 14:23:20 +0100

    the two last fragment well fixed

--- app/src/main/java/cd/amateurmobiledev/leaning/QuestionFragment.kt
+++ app/src/main/java/cd/amateurmobiledev/leaning/QuestionFragment.kt
@@ -5,7 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
+import android.widget.Toast.LENGTH_SHORT
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
 import cd.amateurmobiledev.leaning.databinding.FragmentQuestionBinding
 
 class QuestionFragment : Fragment() {
@@ -16,6 +19,18 @@ class QuestionFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentQuestionBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_question,container,false)
+
+        binding.WinBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_questionFragment_to_winGameFragment)
+            Toast.makeText(activity,"You Win", LENGTH_SHORT).show()
+        }
+
+        binding.LooseBtn.setOnClickListener {
+            it.findNavController().navigate(R.id.action_questionFragment_to_overGameFragment)
+            val looseToast = Toast.makeText(activity, "You Loose",LENGTH_SHORT)
+                looseToast.show()
+        }
+
         return binding.root
     }
 }
