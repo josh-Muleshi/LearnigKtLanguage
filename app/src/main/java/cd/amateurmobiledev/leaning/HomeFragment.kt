e7fc5e6313439802b7fb5bb02ba469519f6fffcc
josh muleshi <jmuleshi2@gmail.com>
2021-04-02 13:24:42 +0100

    now i add an action between two fragment in the navGraph the a activity is call the second one the HostActivity

--- app/src/main/java/cd/amateurmobiledev/leaning/HomeFragment.kt
+++ app/src/main/java/cd/amateurmobiledev/leaning/HomeFragment.kt
@@ -6,6 +6,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
+import androidx.navigation.Navigation
 import cd.amateurmobiledev.leaning.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -17,6 +18,10 @@ class HomeFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         val binding: FragmentHomeBinding = DataBindingUtil.inflate(inflater,R.layout.fragment_home, container, false)
+
+        binding.playBtn.setOnClickListener {
+            Navigation.findNavController(it).navigate(R.id.action_homeFragment_to_questionFragment)
+        }
         return binding.root
     }
 }
