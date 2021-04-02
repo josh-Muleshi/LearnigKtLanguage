9d990dfbe6b40b2d9910756e8bb61abf8f9471d4
josh muleshi <jmuleshi2@gmail.com>
2021-04-02 12:32:54 +0100

    first commit after implementing DataBinding in my app learning kotlin language in android dev

---
+++ app/src/main/java/cd/amateurmobiledev/leaning/MainActivity.kt
@@ -0,0 +1,18 @@
+package cd.amateurmobiledev.leaning
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import cd.amateurmobiledev.leaning.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val binding: ActivityMainBinding = DataBindingUtil.setContentView(this,R.layout.activity_main)
+
+    }
+}
