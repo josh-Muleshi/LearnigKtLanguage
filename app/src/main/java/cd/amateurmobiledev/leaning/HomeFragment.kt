11c14b1d24b5ebd98f26d691b2a1cd8c405d8744
josh muleshi <jmuleshi2@gmail.com>
2021-04-03 21:36:30 +0100

    create a menu by which we will start to call our about Fragment

--- app/src/main/java/cd/amateurmobiledev/leaning/HomeFragment.kt
+++ app/src/main/java/cd/amateurmobiledev/leaning/HomeFragment.kt
@@ -1,13 +1,13 @@
 package cd.amateurmobiledev.leaning
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.navigation.Navigation
 import androidx.navigation.findNavController
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
 import cd.amateurmobiledev.leaning.databinding.FragmentHomeBinding
 
 class HomeFragment : Fragment() {
@@ -23,6 +23,16 @@ class HomeFragment : Fragment() {
         binding.playBtn.setOnClickListener {
             it.findNavController().navigate(R.id.action_homeFragment_to_questionFragment)
         }
+        setHasOptionsMenu(true)
         return binding.root
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.mini_menu, menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return NavigationUI.onNavDestinationSelected(item,requireView().findNavController()) || super.onOptionsItemSelected(item)
     }
 }
